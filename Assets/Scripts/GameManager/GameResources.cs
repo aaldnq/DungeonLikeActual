76938d6d2783daac434db2b86c947c27346c842b
Tutor <DungeonGunnerCourse>
2025-01-19 19:40:21 +0100

    commit message

---
+++ Assets/Scripts/GameManager/GameResources.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.Audio;
+
+public class GameResources : MonoBehaviour
+{
+    private static GameResources instance;
+
+    public static GameResources Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Resources.Load<GameResources>("GameResources");
+            }
+            return instance;
+        }
+    }
+
+    #region Header DUNGEON
+    [Space(10)]
+    [Header("DUNGEON")]
+    #endregion
+    #region Tooltip
+    [Tooltip("Populate with the dungeon RoomNodeTypeListSO")]
+    #endregion
+
+    public RoomNodeTypeListSO roomNodeTypeList;
+
+
+    #region Validation
+#if UNITY_EDITOR
+    // Validate the scriptable object details entered
+    private void OnValidate()
+    {
+        HelperUtilities.ValidateCheckNullValue(this, nameof(roomNodeTypeList), roomNodeTypeList);
+
+    }
+
+#endif
+    #endregion
+}
